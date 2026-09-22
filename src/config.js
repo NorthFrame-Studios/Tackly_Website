ae4bd0aa1aa67d4be33234e6aa244dcc3e16580d
mforbeskr <mforbeskr@outlook.dk>
2026-09-22 15:44:32 +0200

    address update

--- src/config.js
+++ src/config.js
@@ -26,7 +26,7 @@ export const siteConfig = {
     privacyEmail: 'support@tackly.dk',
     dsaEmail: 'support@tackly.dk',
     companyName: 'NorthFrame',
-    address: 'Cikoriegade 52, 2. tv., 2640 Hedehusene, Danmark',
+    address: '2640 Hedehusene, Danmark',
     cvr: '', // Required if the operator is registered in CVR
     phone: '', // Add if this is the normal direct contact channel
   },
